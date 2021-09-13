74e1e0353a4f43264d66adc87c390652b1b107c1
ZHoangZ <hearterzouest99.999@gmail.com>
2021-09-14 00:04:22 +0700

    update information home

--- CourseBackend/SpringBoot/src/main/java/vn/edu/topedu/rest/HomeREST.java
+++ CourseBackend/SpringBoot/src/main/java/vn/edu/topedu/rest/HomeREST.java
@@ -1,5 +1,6 @@
 package vn.edu.topedu.rest;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.edu.topedu.dao.AppUserDAO;
+import vn.edu.topedu.dao.CourseDAO;
 import vn.edu.topedu.entity.AppUser;
 import vn.edu.topedu.response.MessageResponse;
 
@@ -23,6 +26,8 @@ import vn.edu.topedu.response.MessageResponse;
 public class HomeREST {
 	@Autowired
 	AppUserDAO appUserDAO;
+	@Autowired
+	CourseDAO courseDAO;
 
 	@PostMapping(value="/email")
 	@ResponseBody
@@ -69,6 +74,28 @@ public class HomeREST {
 	@PreAuthorize("hasRole('USER')")
 	public ResponseEntity<Object> user() {	
 		return ResponseEntity.ok().body(new MessageResponse("You are USER ROLE.", "Bạn có quyền USER."));
+	}
+	
+	
+	
+	@GetMapping(value="/home")
+	@ResponseBody
+	
+	public ResponseEntity<Object> username(
+			HttpServletRequest httpServletRequest
+			) {	
+		System.out.println("---------------------------------");
+		
+		Long countAppUser = appUserDAO.countAppUser(null);
+		Long countCourse = courseDAO.getCount(-1, null, BigDecimal.valueOf(-1), BigDecimal.valueOf(-1));
+		
+		return ResponseEntity.ok(new Object() {
+			public Object numberUser=countAppUser;
+			public Object numberCourse=countCourse;
+		});
+		
+		//else return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(new MessageResponse("username not exist.", "username không tồn tại trong hệ thống."));
+		
 	}
 	
 }
